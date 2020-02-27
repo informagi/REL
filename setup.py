910c898fcecd6e045d273bf6e99606ca8ba855cb
mickvanhulst <mick.vanhulst@gmail.com>
2020-02-27 11:00:06 +0100

    Build 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='REL',
-     version='0.1',
+     version='0.2',
      # scripts=['dokr'] ,
      author="Johannes Michael",
      author_email="mick.vanhulst@gmail.com",
