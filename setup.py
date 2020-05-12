df16579c3738f27117897a1d5809865bd734d147
KDercksen <mail@koendercksen.com>
2020-05-12 15:34:15 +0200

    fix indentation

--- setup.py
+++ setup.py
@@ -7,22 +7,22 @@ with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 setuptools.setup(
-     name='REL',
-     version='0.0.1',
-     # scripts=['dokr'] ,
-     author="Johannes Michael",
-     author_email="mick.vanhulst@gmail.com",
-     description="Entity Linking package",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/mickvanhulst/rel",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
+    name="REL",
+    version="0.0.1",
+    # scripts=['dokr'] ,
+    author="Johannes Michael",
+    author_email="mick.vanhulst@gmail.com",
+    description="Entity Linking package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mickvanhulst/rel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=required,
     include_package_data=True,
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
