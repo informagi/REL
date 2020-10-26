7695102cdf55b87abc633b6d2a740304c902c1f8
KDercksen <mail@koendercksen.com>
2020-10-26 11:34:39 +0100

    Add some MD tests. Resolves #49

---
+++ tests/test_flair_md.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+from REL.mention_detection import MentionDetection
+from flair.models import SequenceTagger
+
+
+def test_md():
+    # return standard Flair tagger + mention detection object
+    tagger = SequenceTagger.load("ner-fast")
+    md = MentionDetection(Path(__file__).parent, "wiki_test")
+
+    # first test case: repeating sentences
+    sample1 = {"test_doc": ["Fox, Fox. Fox.", []]}
+    resulting_spans1 = {(0, 3), (5, 3), (10, 3)}
+    predictions = md.find_mentions(sample1, tagger)
+    predicted_spans = {
+        (m["pos"], m["end_pos"] - m["pos"]) for m in predictions[0]["test_doc"]
+    }
+    assert resulting_spans1 == predicted_spans
+
+    # second test case: excessive whitespace
+    sample2 = {"test_doc": ["Fox.                Fox.                   Fox.", []]}
+    resulting_spans2 = {(0, 3), (20, 3), (43, 3)}
+    predictions = md.find_mentions(sample2, tagger)
+    predicted_spans = {
+        (m["pos"], m["end_pos"] - m["pos"]) for m in predictions[0]["test_doc"]
+    }
+    assert resulting_spans2 == predicted_spans
